9e03c367d208832c72a2800e169e980548fce328
Grant Cohoe <grant@grantcohoe.com>
2024-02-25 19:42:54 -0500

    improve logging output

--- src/libraries/Logging.ts
+++ src/libraries/Logging.ts
@@ -1,10 +1,21 @@
 import winston from "winston";
 
 /**
+ * Log format. This came straight from their own documentation at
+ * https://github.com/winstonjs/logform#readme
+ */
+const alignedWithColorsAndTime = winston.format.combine(
+  winston.format.colorize(),
+  winston.format.timestamp(),
+  winston.format.align(),
+  winston.format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
+);
+
+/**
  * Common logging constructor
  */
 export const logger = winston.createLogger({
   level: 'info',
-  format: winston.format.combine(winston.format.timestamp(), winston.format.logstash()),
+  format: alignedWithColorsAndTime,
   transports: [new winston.transports.Console({})],
 });
